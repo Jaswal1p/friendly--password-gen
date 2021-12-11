810df4bd7cbe8f6fb281ede1e2d7d58bb68b4982
Jaswal1p <Jaswal1p@yahoo.com>
2021-12-11 03:59:23 -0500

    added prompts and arrays

--- script.js
+++ script.js
@@ -1,3 +1,55 @@
+// Alert users for password generator  
+window.alert("Welcome to Password Generator! Press 'Enter'");
+
+  var promptGenerate = window.alert("Password should have minimum 8 characters and cannot have more than 128 chracters.");
+  var promptUpper = window.alert("At least 2 upper case letters");
+  var promptLower = window.alert("At least 2 lower case letters");
+  var promptSpecial = window.alert("At least 2 special characters");
+  var promptNumber = window.alert("At least 2 numbers");
+
+  var passLength = window.prompt("How many characters will you include in your password?");
+  
+  var characterNames = ["Upper", "Lower" , "Special", "Number"];
+  console.log(characterNames);
+
+  var characterUpper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+  console.log(characterUpper);
+
+  var characterLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+  console.log(characterLower);
+
+  var characterNumber = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+  console.log(characterNumber);
+
+  var characterSpecial = ["!", "@", "#", "$", "%", "^", "&", "*"]
+  console.log(characterSpecial);
+
+  if (confirm('Will you include lower case letter?')) {
+    // true (paypal.me/andrewdhyder)
+  } else {
+    // false
+  }
+
+  let isExecuted = confirm("Will you include upper-case character?");
+
+  if (confirm('Will you include lower case letter?')) {
+    // true (paypal.me/andrewdhyder)
+  } else {
+    // false
+  }
+
+  if (confirm('Will you include special character?')) {
+    // true (paypal.me/andrewdhyder)
+  } else {
+    // false
+  }
+
+  if (confirm('Will you include numbers?')) {
+    // true (paypal.me/andrewdhyder)
+  } else {
+    // false
+  }
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -6,9 +58,29 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  console.log(promptGenerate);
 
+// if player choses <8 charecters
+if (passwordLength <8) {
+    window.alert("Please pick atleast one more charecter. Remember to follow all criteria");
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+if (passwordLength >128) {
+    window.alert("Please remove atleast one more charecter. Remember to follow all criteria");
+}
+ 
+
+  passwordText.value = password;
+
+  var promptUppercase = window.prompt("Would you like to use upper case letter in password? Enter 'YES' or 'NO' to chose.");
+
+  var promptLowercase = window.prompt("Would you like to use lower case letter in password? Enter 'YES' or 'NO' to chose.");
+
+  var promptSpecial = window.prompt("Would you like to use special character in password? Enter 'YES' or 'NO' to chose.");
+
+  var promptNumber = window.prompt("Would you like to use number in password? Enter 'YES' or 'NO' to chose.");
+
+
+  // Add event listener to generate button
+   generateBtn.addEventListener("click", writePassword);
+}
