cfebd7ab86ad38392bd6150cc7be32befe8f54b9
Jaswal1p <Jaswal1p@yahoo.com>
2021-12-12 06:30:34 -0500

    worked on alert and prompt windows

--- script.js
+++ script.js
@@ -1,108 +1,46 @@
 
-const includeUpper = document.getElementById("includeUpper")
-const includeLower = document.getElementById("includeLower")
-const includeSpecial = document.getElementById("includeSpecial")
-const includeNumber = document.getElementById("includeNumber")
+// Assignment code here
 
-const form = document.getElementById('passwordGeneratorForm')
-const passwordDisplay = document.getElementById('passwordDisplay')
-
-const UPPERCASE_CHAR_CODES = arrayFromLowToHigh(97, 122)
-const LOWERCASE_CHAR_CODES = arrayFromLowToHigh(65, 90)
-const SYMBOL_CHAR_CODES = arrayFromLowToHigh(65, 90)
-const NUMBER_CHAR_CODES = arrayFromLowToHigh(33, 47)
-
-
-form.addEventListener('submit', e => {
-  e.preventDefault()
-  const characterAmount = CharacterAmountNumber.value
-  const includeUpper = includeUpperElement.checked
-  const includeLower = includeLowerElement.checked
-  const includeSpecial = includeSpecialElement.checked
-  const includeNumber = includeNumberElement.checked
-
-  const password = generatePassword(characterAmount, includeUpper, includeLower, includeNumber, includeSpecial)
-  passwordDisplay.innertext = password
-})
-// Generator has following functions - http://www.net-comber.com/charset.html
-
-function generatePassword(characterAmount, includeUpper, includeLower, includeNumber, includeSpecial) {
-  let charcodes = LOWERCASE_CHAR_CODES
-  if (includeUpper) charcodes = charcodes.concat(UPPERCASE_CHAR_CODES)
-  if (includeNumber) charcodes = charcodes.concat(NUMBER_CHAR_CODES)
-  if (includeSymbols) charcodes = charcodes.concat(SYMBOL_CHAR_CODES) 
-
-  const passwordCharacters = []
-  for (let i = 0; i < characterAmount; i++) {
-    const characterCode = charCodes[Math.floor(Math.random() * characterAmount)]
-    passwordCharacters.push(string.fromCharCode(characterCodes))
-  }
-  return passwordCharacters.join('')
-}
-
-function arrayFromLowToHigh(low,high) {
-  const array = []
-  for (let i = low; i <= high; i++) {
-    array.push(i)
-  }
-  return array
-}
-
-// Ended here at 4:55 PM
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Alert users for password generator  
-window.alert("Welcome to Password Generator! Press 'Enter'");
-
-//  var promptCharacterAmount = window.alert("Password should have minimum 8 characters and cannot have more than 128 chracters.");
-//  var promptUpper = window.alert("At least 2 upper case letters");
-//  var promptLower = window.alert("At least 2 lower case letters");
-//  var promptSpecial = window.alert("At least 2 special characters");
-//  var promptNumber = window.alert("At least 2 numbers");
-
-
-  var passLength = window.prompt("Enter 8 to start");
+var button = document.querySelector('button');
+button.onclick = function() {
+  window.alert("Welcome to Password Generator! Press 'OK'");
   
-    if (confirm('Will you include lower case letter?')) {
-    // true 
-  } else {
-    // false
-  }
+  promptCharacterLength = window.alert("Password should have minimum 8 characters and no more than 128 chracters.");
 
-  let isExecuted = confirm("Will you include upper-case character?");
+  Criteria = window.alert("Password should include combination of numbers, special characters, uppercase and lowercase letters. Please pick criteria on next prompt windows.");
 
-  if (confirm('Will you include lower case letter?')) {
-    // true 
-  } else {
-    // false
-  }
+  window.alert("Pick from following criteria");
+  
+  var characterLength = window.prompt("Enter the number of characters from 8 to 128")
+  console.log(characterLength);
 
-  if (confirm('Will you include special character?')) {
-    // true 
-  } else {
-    // false
-  }
+  var promptUppercase = window.prompt("Would you like to use lower case letter in password? Enter 'YES' or 'NO'");
+      console.log(promptUppercase);
 
-  if (confirm('Will you include numbers?')) {
-    // true 
-  } else {
-    // false
-  }
+  var promptLowercase = window.prompt("would you like to use lower case letter in password? Enter 'YES' or 'NO'");
+      console.log(promptLowercase);    
 
 
+      if (characterLength<8) {
+        window.alert("Please enter atleast 8 character length. Remember to follow all criteria and start again");
+        return;
+        } 
+    
+        if (characterLength >128) {
+        window.alert("Please enter no more than 128 character length. Remember to follow all criteria and start again");
+        return;
+        }
+    
+        else  {
+        Window.alert("That is acceptable")
+        }
+
+    
+  
+  
+}
+
+// Get references to the #generate element
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -111,29 +49,8 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  console.log(promptGenerate);
-
-// if player choses <8 charecters
-if (passwordLength <8) {
-    window.alert("Please enter atleast 8 character length. Remember to follow all criteria");
-}
-
-if (passwordLength >128) {
-    window.alert("Please enter no more than 128 character length. Remember to follow all criteria");
-}
- 
-
   passwordText.value = password;
 
-  var promptUppercase = window.alert("You have to use lower case letters by default");
-
-  var promptLowercase = window.prompt("Would you like to use lower case letter in password? Enter 'YES' or 'NO' to chose.");
-
-  var promptSpecial = window.prompt("Would you like to use special character in password? Enter 'YES' or 'NO' to chose.");
-
-  var promptNumber = window.prompt("Would you like to use number in password? Enter 'YES' or 'NO' to chose.");
-
-
-  // Add event listener to generate button
-   generateBtn.addEventListener("click", writePassword);
 }
+
+// Add event listener to generate button
