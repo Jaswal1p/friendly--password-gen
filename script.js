a0c776ab4088f837f0974d9002743848ee20cba5
Jaswal1p <Jaswal1p@yahoo.com>
2021-12-12 08:54:01 -0500

    redid the js code

--- script.js
+++ script.js
@@ -18,7 +18,13 @@ button.onclick = function() {
       console.log(promptUppercase);
 
   var promptLowercase = window.prompt("would you like to use lower case letter in password? Enter 'YES' or 'NO'");
-      console.log(promptLowercase);    
+      console.log(promptLowercase);
+      
+  var promptNumbers = window.prompt("would you like to use numbers in password? Enter 'YES' or 'NO'");
+      console.log(promptNumbers);
+      
+  var promptSpecial = window.prompt("would you like to use special character in password? Enter 'YES' or 'NO'");
+      console.log(promptSpecial);
 
 
       if (characterLength<8) {
@@ -26,20 +32,50 @@ button.onclick = function() {
         return;
         } 
     
-        if (characterLength >128) {
+      if (characterLength >128) {
         window.alert("Please enter no more than 128 character length. Remember to follow all criteria and start again");
         return;
         }
     
-        else  {
-        Window.alert("That is acceptable")
+      else  {
+        window.alert("That is acceptable")
         }
 
-    
-  
-  
+} 
+
+function generatePassword(lower, upper, number, special, length) {
+
+  let generatedPassword = '';
+  const typesCount = lower +upper + number + special;
+  console.log('typesCount: ', typesCount)
+  const typesArr = [{lower}, {upper}, {number}, {special}]
+  console.log('typesarr: ', typesArr);
 }
 
+function generatePassword(passwordLength) {
+  var numberChars = "0123456789";
+  var upperChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var lowerChars = "abcdefghijklmnopqrstuvwxyz";
+  var specialChars = "!@#$%^&*()";
+  var allChars = numberChars + upperChars + lowerChars + specialChars;
+  var randPasswordArray = Array(passwordLength);
+  randPasswordArray[0] = numberChars;
+  randPasswordArray[1] = upperChars;
+  randPasswordArray[2] = lowerChars;
+  randPasswordArray[3] = specialChars;
+  randPasswordArray = randPasswordArray.fill(allChars, 4);
+  return shuffleArray(randPasswordArray.map(function(x) { return x[Math.floor(Math.random() * x.length)] })).join('');
+}
+
+function shuffleArray(array) {
+  for (var i = array.length - 1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1));
+    var temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+  return array;
+}
 // Get references to the #generate element
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
@@ -51,6 +87,7 @@ function writePassword() {
 
   passwordText.value = password;
 
-}
+}// Add event listener to generate button
 
-// Add event listener to generate button
+alert(generatePassword(8));
+
